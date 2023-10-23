a2310910b81ef7f8577a9f5f2bbcf51da9170a51
CB97103 <gxguaqueta@soy.sena.edu.co>
2023-10-23 12:45:57 -0500

    Creación de tarjetas

--- src/view.js
+++ src/view.js
@@ -1,5 +1,25 @@
 export const renderItems = (data) => {
-  console.log(data)
+  console.log(data);
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  const cardsItem = document.querySelector(".cards");
+  let showInHtml = "";
+  data.forEach((items) => {
+    showInHtml += `
+      <div class="itemContainer">
+          <dl itemscope itemtype="WomenInTech">
+            <img src="${items.imageUrl}" alt="" />
+            <dt>Nombre:</dt><dd itemprop="name">${items.name}</dd>
+            <dt>Descripción:</dt><dd itemprop="shortDescription">${items.shortDescription}</dd>
+            <dt>Año:</dt><dd itemprop="shortDescription">${items.facts.year}</dd>
+            <dt>Genero:</dt><dd itemprop="shortDescription">${items.facts.genre}</dd>
+            <dt>Esudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
+          </dl>
+        </div>
+    `;
+  });
+
+  cardsItem.innerHTML = showInHtml;
+
+  return "example";
 };
