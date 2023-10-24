b77ed6988ca559afe774d33b5d3090be609455c6
CB97103 <gxguaqueta@soy.sena.edu.co>
2023-10-24 12:22:35 -0500

    Avance de tarjetas en HTML

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+const cardsContainer = document.querySelector("#root")
+  cardsContainer.innerHTML = renderItems(data);
+
 console.log(example, renderItems(data), data);
--- src/view.js
+++ src/view.js
@@ -1,13 +1,12 @@
 export const renderItems = (data) => {
   console.log(data);
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-
-  const cardsItem = document.querySelector(".cards");
   let showInHtml = "";
   data.forEach((items) => {
     showInHtml += `
-      <div class="itemContainer">
-          <dl itemscope itemtype="WomenInTech">
+
+      <li class="itemContainer">
+            <dl itemscope itemtype="WomenInTech">
             <img src="${items.imageUrl}" alt="" />
             <dt>Nombre:</dt><dd itemprop="name">${items.name}</dd>
             <dt>Descripción:</dt><dd itemprop="shortDescription">${items.shortDescription}</dd>
@@ -15,11 +14,9 @@ export const renderItems = (data) => {
             <dt>Genero:</dt><dd itemprop="shortDescription">${items.facts.genre}</dd>
             <dt>Esudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
           </dl>
-        </div>
+        </li>
     `;
   });
 
-  cardsItem.innerHTML = showInHtml;
-
-  return "example";
+  return `<ul class="cards">${showInHtml}</ul>`;
 };
