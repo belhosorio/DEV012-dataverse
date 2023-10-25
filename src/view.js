cd253b270a06a73a63e7d6bea62e708433534f1b
CB97103 <gxguaqueta@soy.sena.edu.co>
2023-10-24 21:21:44 -0500

    Cambios CSS

--- src/view.js
+++ src/view.js
@@ -5,15 +5,17 @@ export const renderItems = (data) => {
   data.forEach((items) => {
     showInHtml += `
 
-      <li class="itemContainer">
+      <li class="itemContainer"><section>
             <dl itemscope itemtype="WomenInTech">
             <img src="${items.imageUrl}" alt="" />
+            
             <dt>Nombre:</dt><dd itemprop="name">${items.name}</dd>
             <dt>Descripción:</dt><dd itemprop="shortDescription">${items.shortDescription}</dd>
             <dt>Año:</dt><dd itemprop="shortDescription">${items.facts.year}</dd>
             <dt>Genero:</dt><dd itemprop="shortDescription">${items.facts.genre}</dd>
             <dt>Esudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
-          </dl>
+          
+            </dl>  <section>
         </li>
     `;
   });
