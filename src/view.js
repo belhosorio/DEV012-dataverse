cd32b5cca89931aa5285a739308c073dcaaa8107
CB97103 <gxguaqueta@soy.sena.edu.co>
2023-10-25 13:18:14 -0500

    Error CSS

--- src/view.js
+++ src/view.js
@@ -6,13 +6,13 @@ export const renderItems = (data) => {
     showInHtml += `
 <ul class="cards">
       <li class="itemContainer">
-            <dl itemscope itemtype="WomenInTech">
+            <dl itemscope itemtype="kaonashiPelis">
             <img src="${items.imageUrl}" alt="" />  
             <dt>Nombre:</dt><dd itemprop="name">${items.name}</dd>
             <dt>Descripción:</dt><dd itemprop="shortDescription">${items.shortDescription}</dd>
-            <dt>Año:</dt><dd itemprop="shortDescription">${items.facts.year}</dd>
-            <dt>Genero:</dt><dd itemprop="shortDescription">${items.facts.genre}</dd>
-            <dt>Esudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
+            <dt>Año:</dt><dd itemprop="year">${items.facts.year}</dd>
+            <dt>Genero:</dt><dd itemprop="genre">${items.facts.genre}</dd>
+            <dt>Esudio de animación:</dt><dd itemprop="studio">${items.facts.studio}</dd>
             </dl>  <section>
         </li>
         </ul>
