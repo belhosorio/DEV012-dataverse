5231523df249e21b44f3aaca9ba17c48f697d5c6
belh osorio <belh.osoriocom@gmail.com>
2023-10-26 14:39:37 -0300

    OH con JC

--- src/view.js
+++ src/view.js
@@ -7,13 +7,13 @@ export const renderItems = (data) => {
     showInHtml += `
 <ul class="cards">
       <li class="itemContainer">
-            <dl itemscope itemtype="WomenInTech">
+            <dl itemscope itemtype="PeliculasAnimacionJaponesa">
             <img src="${items.imageUrl}" alt="" />
             <dt>Nombre:</dt><dd itemprop="name">${items.name}</dd>
             <dt>Descripción:</dt><dd itemprop="shortDescription">${items.shortDescription}</dd>
             <dt>Año:</dt><dd itemprop="shortDescription">${items.facts.year}</dd>
             <dt>Genero:</dt><dd itemprop="shortDescription">${items.facts.genre}</dd>
-            <dt>Esudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
+            <dt>Estudio de animación:</dt><dd itemprop="shortDescription">${items.facts.studio}</dd>
           </dl>
         </li>
         </ul>
