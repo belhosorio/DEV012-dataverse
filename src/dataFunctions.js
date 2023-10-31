1877a48e592547cc553398ec41a474b4e686f43a
belh osorio <belh.osoriocom@gmail.com>
2023-10-31 00:41:18 -0300

    error al bajar filtro

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -5,59 +5,38 @@
 
 
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
-};
-
-/*
-
-export const genreFilter = renderItems.filter(function(data, filterBy, value ){
-  return renderItems.genre === genre
-});
-
-
-export const nameFilter = () => {
-  filterByID(data, filterBy, value)
 
 
 
-sintaxis
-var newArray = arr.filter(callback(currentValue[, index[, array]])[, thisArg])
 
 
 
-ejemplo 1
-/* const arr = [
-  { id: 15 },
-  { id: -1 },
-  { id: 0 },
-  { id: 3 },
-  { id: 12.2 },
-  {},
-  { id: null },
-  { id: NaN },
-  { id: "undefined" },
-];
 
-let invalidEntries = 0;
 
-function filterByID(item) {
-  if (Number.isFinite(item.id) && item.id !== 0) {
-    return true;
+
+
+
+
+
+
+
+
+
+
+
+
+export const filterGenre = (data, filterBy, value) => {
+
+if(filterBy === "genre"){
+  const resultado = [];
+  for(let i = 0; i < data.length; i++){
+    const generoActual = data[i].facts.genre;
+    if(generoActual ===  value){
+      resultado.push(data[i])
+    }
   }
-  invalidEntries++;
-  return false;
+  return resultado;
 }
 
-const arrByID = arr.filter(filterByID);
+};
 
-console.log("Filtered Array\n", arrByID);
-// Filtered Array
-// [{ id: 15 }, { id: -1 }, { id: 3 }, { id: 12.2 }]
-
-console.log("Number of Invalid Entries =", invalidEntries);
-// Number of Invalid Entries = 5 */
--- src/main.js
+++ src/main.js
@@ -3,12 +3,30 @@ import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
 
+console.log(example, renderItems(data), data);
+
 const cardsContainer = document.querySelector("#root");
 cardsContainer.innerHTML = renderItems(data);
 
-console.log(example, renderItems(data), data);
+
+
+
 
 const clearButton = document.getElementById("button");
 clearButton.addEventListener("click", function () {
   cardsContainer.innerHTML = renderItems(data);
 });
+
+/*main
+
+  datosFiltrados.forEach(function(renderItems) {
+        let genero = document.createElement('option[value="genero-op"]');
+        let accion = document.createElement('option[value="action"]');
+        accion.innerHTML = showInHtml.accion;
+      accion.innerHTML = items.accion;
+    };
+
+function filtrarDatos() {
+    let filtrogenero = document.querySelector('label[for="genre"]').value;
+    mostrarDatos(filtrogenero);
+};
